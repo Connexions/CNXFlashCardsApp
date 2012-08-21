c323ddd3dcb0e9c1ab6ad25a34535b01db4b2c1a
withad <armcorbett@gmail.com>
2012-08-21 15:06:29 +0100

    Removed some unused imports.

--- CnxFlashCards/src/org/cnx/quizcards/database/CardProvider.java
+++ CnxFlashCards/src/org/cnx/quizcards/database/CardProvider.java
@@ -8,14 +8,11 @@
 package org.cnx.quizcards.database;
 
 import static org.cnx.quizcards.Constants.CARDS_TABLE;
-import static org.cnx.quizcards.Constants.DECKS_TABLE;
-import static org.cnx.quizcards.Constants.TAG;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.util.Log;
 
 public class CardProvider extends ContentProvider {
 
